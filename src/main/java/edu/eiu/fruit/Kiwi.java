bb16488a2e95df98df0f88e0302833eb070f350e
PretPC <nhokpret.koolboy@gmail.com>
2017-10-06 14:33:59 +0700

    Adding kiwi

---
+++ src/main/java/edu/eiu/fruit/Kiwi.java
@@ -0,0 +1,11 @@
+package edu.eiu.fruit;
+
+public class Kiwi implements IFruit {
+    public String getName() {
+        return "Kiwi";
+    }
+
+    public int getRollMultiplier() {
+        return 0;
+    }
+}
